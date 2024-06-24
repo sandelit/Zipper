89f1514dc4a15775cf5be34c391652cb02e96785
sandelit <thomas.sandelin@gmail.com>
2024-06-24 22:53:46 +0300

    initial commit

---
+++ src/main/java/com/file/zipper/ZipperApplication.java
@@ -0,0 +1,13 @@
+package com.file.zipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/file/zipper/ZipperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file.zipper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
